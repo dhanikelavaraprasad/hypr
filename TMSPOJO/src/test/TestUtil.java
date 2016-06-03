a0e3e000f20453c035ac83cb050aec94c61c8951
Varaprasad <dhanikelavaraprasad@gmail.com>
2016-06-03 08:17:23 +0530

    Updated entities

--- TMSPOJO/src/test/TestUtil.java
+++ TMSPOJO/src/test/TestUtil.java
@@ -2,14 +2,18 @@ package test;
 
 import java.util.List;
 
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Projection;
+import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
 
-import com.cyb.tms.entity.TmsSprintMst;
+import com.cyb.tms.entity.TmsStoryMst;
 import com.cyb.tms.entity.TmsUsers;
+import com.cyb.tms.entity.UserStoryStaus;
 
 
 
@@ -32,14 +36,28 @@ public class TestUtil {
 	                .add(Restrictions.eq("sp.sprintStatus", "OPEN")).uniqueResult();
 			System.out.println(sprint);*/
 			
-			@SuppressWarnings("unchecked")
+			/*@SuppressWarnings("unchecked")
 			List<TmsUsers> users = session.createCriteria(TmsUsers.class, "tu")
 					               .createAlias("tmsProject", "proj")
 					               .add(Restrictions.eqProperty("tu.tmsProject", "proj.pid"))
 				                   .add(Restrictions.eq("proj.pid", new Long(1)))
-				                   .add(Restrictions.eq("tu.isActive", "ACTIVE")).list();
+				                   .add(Restrictions.eq("tu.isActive", "ACTIVE")).list();*/
 			
-			System.out.println(users.size());
+			/*System.out.println(users.size());*/
+			
+		@SuppressWarnings("unchecked")
+		List<String> jiraIds = session.createCriteria(UserStoryStaus.class, "uss")
+								.createAlias("tmsSprintMst", "sprint")
+								.setProjection(Projections.projectionList().add(Projections.property("jiraId")))
+								//.add(Restrictions.eqProperty("uss.tmsSprintMst", "sprint.sprintId"))
+								.add(Restrictions.eq("sprint.sprintId", new Long(1))).list();
+			
+			
+//			Criteria criteria = session.createCriteria(TmsStoryMst.class, "story");
+//			criteria.createAlias("userStoryStatus", "uss");
+//			criteria.add(Restrictions.eqProperty("uss.jiraId", "story.jiraId"));
+//			criteria.add(Restrictions.eq("uss.tmsSprintMst", new Long(1)));
+			System.out.println(jiraIds);
 			
 //			TmsProject project = new TmsProject();
 //			project.setName("DOMO");
